486c8b48192b3e371df4bbb51539a3c7428ef1d3
Vector-98 <64809119+Vector-98@users.noreply.github.com>
2021-02-05 17:25:17 -0600

    Update Cleaner API.user.js

--- Cleaner API.user.js
+++ Cleaner API.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cleaner API
 // @namespace    http://tampermonkey.net/
-// @version      1.2.89
+// @version      1.2.88
 // @updateURL    https://github.com/Vector-98/CleanAPI/raw/master/Cleaner%20API.user.js
 // @downloadURL  https://github.com/Vector-98/CleanAPI/raw/master/Cleaner%20API.user.js
 // @description  try to make things better for everyone
